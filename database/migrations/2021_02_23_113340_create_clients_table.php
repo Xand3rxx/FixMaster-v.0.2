2747f568fcbe2226792984c3dc083ebd3c9b3ba3
Adeolu <osunsamiadeolu@gmail.com>
2021-02-23 04:02:05 -0800

    Clients Seeder
    Created Clients Table and Seeder

---
+++ database/migrations/2021_02_23_113340_create_clients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->id();
+
+            $table->foreignId('user_id')->index();
+            $table->foreignId('account_id')->index();
+            $table->foreignId('estate_id')->index();
+            $table->foreignId('profession_id')->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Client User
+        $client = new \App\Models\User;
+        $client->email = 'client@fix-master.com';
+        $client->password = bcrypt('admin12345');
+        $client->save();
+
+        // Client Roles and Permissions
+        $clientRole = \App\Models\Role::where('slug', 'client-user')->first();
+        $client->roles()->attach($clientRole);
+
+        $client_permission = \App\Models\Permission::where('slug', 'view-clients')->first();
+        $client->permissions()->attach($client_permission);
+
+        // Client User Type
+        $clientType = new \App\Models\UserType();
+        $clientType->user_id = $client->id;
+        $clientType->role_id = $clientRole->id;
+        $clientType->url = $clientRole->url;
+        $clientType->save();
+
+        // Client Account
+        $clientAccount = \App\Models\Account::create([
+            'user_id' =>  $client->id,
+            'first_name' => "Client",
+            'gender' => 'others'
+        ]);
+
+        // Client Table
+        $cseTable = new \App\Models\Client();
+        $cseTable->user_id = $client->id;
+        $cseTable->account_id = $clientAccount->id;
+        $cseTable->estate_id = '1';
+        $cseTable->	profession_id = '1';
+        $cseTable->save();
+    }
+}
