3fe24b6e5c5272152de415396f1804f68e92014d
Adeolu <osunsamiadeolu@gmail.com>
2021-03-14 10:18:28 -0700

    Payment Transaction Trait
    Denk i have made Adjustment to walletSubmit method and created a payment transaction trait

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -5,28 +5,28 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Models\Category; 
+use App\Models\Category;
 use App\Models\Service;
-use App\Models\PaymentGateway; 
+use App\Models\PaymentGateway;
 use App\Models\Payment;
 use App\Models\WalletTransaction;
 use App\Models\User;
 use App\Helpers\CustomHelpers;
+use App\Traits\RegisterPaymentTransaction;
 use Session;
 
 class ClientController extends Controller
 {
+    use RegisterPaymentTransaction;
 
 
     //call the profile page with credentials
     public function edit_profile(Request $request)
     {
-
     }
 
     public function update_profile(Request $request)
     {
-      
     }
 
     /**
@@ -34,16 +34,14 @@ class ClientController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function updatePassword(Request $request){
-
-
+    public function updatePassword(Request $request)
+    {
     }
 
-    public function view_profile(Request $request){
-       
-
+    public function view_profile(Request $request)
+    {
     }
-    
+
     /**
      * Display a listing of the resource.
      *
@@ -51,9 +49,9 @@ class ClientController extends Controller
      */
     public function index()
     {
-        
+
         $popularRequests = Service::select('id', 'name', 'url', 'image')->take(10)->get()->random(3);
-        
+
         return view('client.home', [
             // data
             'totalRequests' => rand(1, 1),
@@ -144,63 +142,74 @@ class ClientController extends Controller
 
     public function wallet()
     {
-        $data['title']        ='Fund your wallet';
-        $data['gateways']     =PaymentGateway::whereStatus(1)->orderBy('id', 'DESC')->get();
+        $data['title']        = 'Fund your wallet';
+        $data['gateways']     = PaymentGateway::whereStatus(1)->orderBy('id', 'DESC')->get();
         return view('client.wallet', $data);
     }
-        	    
-    
+
+
     public function walletSubmit(Request $request)
-    { 
+    {
+        // validate Request
+        $valid = $this->validate($request, [
+            // List of things needed from the request like 
+            // Amount, Payment Channel, Payment for, Reference Id
+        ]);
+        // fetch the Client Table Record
+        $client = \App\Models\Client::where('user_id', $request->user()->id)->with('user')->first();
+        // call the payment Trait and submit record on the 
+        $payment = $this->payment($valid['amount'], $valid['payment_channel'], $valid['payment_for'], $client['unique_id'], 'pending', $valid['reference_id']);
+
         // try {
-        $user = User::find(auth()->user()->id);
-        
-        $depo = new Payment();
-        $depo['user_id'] = auth()->user()->id;
-        $depo['amount'] = $request->amount;
-        $depo['payment_channel'] = 1; 
-        $depo['payment_for'] = 1;
-        $depo['unique_id'] = $user->wallet_id; //wallet ID
-        $CustomHelper = new CustomHelpers();
-        $depo['reference_id'] =  $CustomHelper->generateRandomNumber(); 
-        $depo['status'] = 'pending';        
-        Session::put('Track', $depo['reference_id']); 
+        // $user = User::find(auth()->user()->id);
 
-        $gate = PaymentGateway::where('id', $request->gateway)->first();
+        // $depo = new Payment();
+        // $depo['user_id'] = auth()->user()->id;
+        // $depo['amount'] = $request->amount;
+        // $depo['payment_channel'] = 1; 
+        // $depo['payment_for'] = 1;
+        // $depo['unique_id'] = $user->wallet_id; //wallet ID
+        // $CustomHelper = new CustomHelpers();
+        // $depo['reference_id'] =  $CustomHelper->generateRandomNumber(); 
+        // $depo['status'] = 'pending';        
+        // Session::put('Track', $depo['reference_id']); 
 
-        // if is already a made a transaction we already have his/her wallet id; then update
-        if (!WalletTransaction::where('unique_id', '=', $user->wallet_id)->exists()) {
+        // $gate = PaymentGateway::where('id', $request->gateway)->first();
 
-              
-                if ($depo->save()) {
-                    $track = Session::get('Track');           
-                    $data  = Payment::where('reference_id', $track)->orderBy('id', 'DESC')->first();            
-                    
-                    // $walTrans = new WalletTransaction;
-        
-                    $walTrans['user_id'] = auth()->user()->id;
-                    $walTrans['payment_id'] = $data->id;
-                    $walTrans['amount'] = $data->amount;
-                    $walTrans['payment_type'] = 1;
-                    $walTrans['unique_id'] = $data->unique_id;
-                    $walTrans['transaction_type'] = 1;  
-                    if (WalletTransaction::create($walTrans)) {
-                        $this->directToRightpage();
-                                     
-                    } 
-                    
-                }
+        // // if is already a made a transaction we already have his/her wallet id; then update
+        // if (!WalletTransaction::where('unique_id', '=', $user->wallet_id)->exists()) {
 
-         }else{
-            $this->directToRightpage();
-         }
+
+        //         if ($depo->save()) {
+        //             $track = Session::get('Track');           
+        //             $data  = Payment::where('reference_id', $track)->orderBy('id', 'DESC')->first();            
+
+        //             // $walTrans = new WalletTransaction;
+
+        //             $walTrans['user_id'] = auth()->user()->id;
+        //             $walTrans['payment_id'] = $data->id;
+        //             $walTrans['amount'] = $data->amount;
+        //             $walTrans['payment_type'] = 1;
+        //             $walTrans['unique_id'] = $data->unique_id;
+        //             $walTrans['transaction_type'] = 1;  
+        //             if (WalletTransaction::create($walTrans)) {
+        //                 $this->directToRightpage();
+
+        //             } 
+
+        //         }
+
+        //  }else{
+        //     $this->directToRightpage();
+        //  }
 
 
 
     }
 
 
-    public function directToRightpage(){
+    public function directToRightpage()
+    {
         $user_id = auth()->user()->id;
         $track = Session::get('Track');
         $pay =  Payment::where('reference_id', $track)->orderBy('id', 'DESC')->first();
@@ -212,22 +221,22 @@ class ClientController extends Controller
             return redirect()->route('client.wallet', app()->getLocale())->with('alert', 'Invalid Deposit Request');
         }
         $gatewayData = PaymentGateway::where('id', $data->payment_channel)->first();
-        
+
         if ($pay->payment_channel == 1) {
             $paystack['amount'] = $pay->amount;
             $paystack['track'] = $track;
             $title = $gatewayData->name;
             return view('client.payment.paystack', compact('paystack', 'title', 'gatewayData', 'data'));
-           
-        }elseif ($pay->payment_channel == 2) {
+        } elseif ($pay->payment_channel == 2) {
             $flutter['amount'] = $pay->amount;
             $flutter['track'] = $track;
             $title = $gatewayData->name;
             return view('client.payment.flutter', compact('flutter', 'title', 'gatewayData', 'data'));
-        } 
+        }
     }
 
-    public function paystackIPN(Request $request) {
+    public function paystackIPN(Request $request)
+    {
         $track  = Session::get('Track');
 
         $data = Payment::where('reference_id', $track)->orderBy('id', 'DESC')->first();
@@ -245,7 +254,7 @@ class ClientController extends Controller
                 'callback_url' => route('client.ipn.paystackApiRequest', app()->getLocale())
             ]),
             CURLOPT_HTTPHEADER => [
-                "authorization: Bearer sk_test_b612f25bd992c4d84760e312175c7515336b77fc" ,
+                "authorization: Bearer sk_test_b612f25bd992c4d84760e312175c7515336b77fc",
                 "content-type: application/json",
                 "cache-control: no-cache"
             ],
@@ -263,13 +272,13 @@ class ClientController extends Controller
             return back()->with('error', $tranx['message']);
         }
         return redirect($tranx['data']['authorization_url']);
-
     }
 
 
 
 
-    public function apiRequest(){
+    public function apiRequest()
+    {
         $track  = Session::get('Track');
         $data = Payment::where('reference_id', $track)->orderBy('id', 'DESC')->first();
 
@@ -277,54 +286,50 @@ class ClientController extends Controller
 
         /** Check for a reference and return else make empty */
         $reference = isset($_GET['reference']) ? $_GET['reference'] : '';
-        if(!$reference){
+        if (!$reference) {
             die('No reference supplied');
         }
 
         /** Set the client for url's array values for the Curl's */
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.paystack.co/transaction/verify/".rawurlencode($reference),
+            CURLOPT_URL => "https://api.paystack.co/transaction/verify/" . rawurlencode($reference),
             CURLOPT_RETURNTRANSFER => true,
 
             /** Set the client for url header values passed */
             CURLOPT_HTTPHEADER => [
-            "accept: application/json",
-            "authorization: Bearer sk_test_b612f25bd992c4d84760e312175c7515336b77fc",
-            "cache-control: no-cache"
+                "accept: application/json",
+                "authorization: Bearer sk_test_b612f25bd992c4d84760e312175c7515336b77fc",
+                "cache-control: no-cache"
             ],
-            ));
+        ));
 
-            /** The response should be executed if successful */
-            $response = curl_exec($curl);
+        /** The response should be executed if successful */
+        $response = curl_exec($curl);
 
-            /** If there's an error return the error message */
-            $err = curl_error($curl);
+        /** If there's an error return the error message */
+        $err = curl_error($curl);
 
-            if($err){
-                print_r('Api returned error '. $err);
-            }
+        if ($err) {
+            print_r('Api returned error ' . $err);
+        }
 
-            /** The transaction details and stats would be returned */
-            $trans = json_decode($response);
-            if(!$trans->status){
-                die('Api returned Error '. $trans->message);
-            }
+        /** The transaction details and stats would be returned */
+        $trans = json_decode($response);
+        if (!$trans->status) {
+            die('Api returned Error ' . $trans->message);
+        }
 
-            /** If the transaction stats are successful snd to DB */
-            if ($data->status == 0) {
-                $data['status'] = 1;
-                $data['transaction_id'] = rawurlencode($reference);
-                $data->update();
-                $walTrans = WalletTransaction::find($data->user_id);
-                $walTrans['balance'] = $walTrans->balance + $data->amount;
-                $user->update();      
-            }
+        /** If the transaction stats are successful snd to DB */
+        if ($data->status == 0) {
+            $data['status'] = 1;
+            $data['transaction_id'] = rawurlencode($reference);
+            $data->update();
+            $walTrans = WalletTransaction::find($data->user_id);
+            $walTrans['balance'] = $walTrans->balance + $data->amount;
+            $user->update();
+        }
 
-            /** Finally return the callback view for the end user */            
-            return redirect()->route('user.fund')->with('success', 'Fund successfully added!');
-       
+        /** Finally return the callback view for the end user */
+        return redirect()->route('user.fund')->with('success', 'Fund successfully added!');
     }
-
-
-
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Payment extends Model
 {
-    use HasFactory;
     protected $fillable = [
         'user_id',
         'amount',
@@ -17,4 +17,20 @@ class Payment extends Model
         'reference_id',
         'status',
     ];
+
+    public const PAYMENT_E_WALLET = 'e-wallet';
+    public const PAYMENT_SERVICE_REQUEST = 'service-request';
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($payment) {
+            $payment->user_id = auth()->user()->id; // Register the User making the payment transaction
+        });
+    }
+
 }
