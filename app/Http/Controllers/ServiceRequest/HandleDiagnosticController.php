2365f822cabf5e1bb58d0a8166e6b7f1da5e4141
isaacola10 <isaacolaolu10@gmail.com>
2021-04-04 15:49:24 +0100

    Isaac Commit

---
+++ app/Http/Controllers/ServiceRequest/HandleDiagnosticController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\ServiceRequest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HandleDiagnosticController extends Controller
+{
+    public function generateDiagnosisInvoice($serviceRequest, $subStatus)
+    {
+
+    }
+}
