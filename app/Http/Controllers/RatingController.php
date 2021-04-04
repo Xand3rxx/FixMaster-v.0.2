7f51722c258bbc5935068e5ed334b7adc450eddd
Adeolu <osunsamiadeolu@gmail.com>
2021-04-03 18:18:32 -0700

    Update RatingController.php

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Review;
 use App\Models\Rating;
+use App\Models\Review;
+use Illuminate\Http\Request;
 use App\Models\ServiceRequest;
+use Illuminate\Support\Facades\DB;
 
 class RatingController extends Controller
 {
@@ -21,8 +22,10 @@ class RatingController extends Controller
     {
         //dd($request->all());
         (array) $valid = $this->validateRatingsRequest($request);
-    //return $request->clientStar."<br>".$request->getClient."<br>".$request->star."<br>".$request->usersIdentity."<br>".$request->serviceRequestId;
-        return self::store($valid, $request->user()->id);
+        //return $request->clientStar."<br>".$request->getClient."<br>".$request->star."<br>".$request->usersIdentity."<br>".$request->serviceRequestId;
+        return self::store($valid, $request->user()->id) == true
+            ? back()->withSuccess('Thank you for rating the request')
+            : back()->with('error', 'Error occured while recording rating the request');
     }
 
     public function handleServiceRatings(Request $request)
@@ -31,29 +34,47 @@ class RatingController extends Controller
         //return back()->withSuccess('Thank you for rating the request');
 
         return response()->json("Yea, It is updated");
-
     }
 
-    protected static function store(array $valid, int $user_id){
+    protected static function store(array $valid, int $user_id)
+    {
+        (bool) $registred = false;
 
-          //if(!empty($valid['review'])){
-        foreach ($valid['users_id'] as $key => $user) {
+        DB::transaction(function () use ($valid, $user_id, &$registred) {
+            // Record each User ratings
+            foreach ($valid['users_id'] as $key => $user) {
+                Rating::create([
+                    'user_id' => $user_id,
+                    'ratee_id' => $user,
+                    'service_request_id' => $valid['serviceRequestId'],
+                    'star' => $valid['users_star'][$key],
+                ]);
+            }
+            // Record Client Ratings
             Rating::create([
                 'user_id' => $user_id,
-                'ratee_id' => $user,
+                'ratee_id' => $valid['client_id'],
                 'service_request_id' => $valid['serviceRequestId'],
-                'star' => $valid['users_star'][$key],
+                'star' => $valid['client_star'],
             ]);
-        }
+            // Update the service request to indicate rated
+            ServiceRequest::where('id', $valid['serviceRequestId'])->first()->update(['has_cse_rated' => 'Yes']);
+            // update registered to be true
+            $registred = true;
+        });
+        return $registred;
 
-            //   Review::create([
-            //     'user_id' => $request->user->id,
-            //     'service_id' => $valid['cse_id'],
-            //     'review' => $valid['review'],
-            // ]);
-          ServiceRequest::where('id', $valid['serviceRequestId'])->first()->update(['has_cse_rated' => 'Yes']);
-            return back()->withSuccess('Thank you for rating the request');
-         // }
+        //if(!empty($valid['review'])){
+
+
+        //   Review::create([
+        //     'user_id' => $request->user->id,
+        //     'service_id' => $valid['cse_id'],
+        //     'review' => $valid['review'],
+        // ]);
+
+
+        // }
 
     }
 
@@ -67,8 +88,8 @@ class RatingController extends Controller
      */
     protected function validateRatingsRequest(Request $request)
     {
-        return $this->validate($request,[
-           // 'review' => 'required|max:255',
+        return $this->validate($request, [
+            // 'review' => 'required|max:255',
             'client_star' => 'required|numeric|between:1,5',
             'client_id' => 'required|numeric',
             'users_id' => 'required|array',
@@ -86,11 +107,10 @@ class RatingController extends Controller
         ]);
     }
 
-    public function getServiceRatings(Request $request){
+    public function getServiceRatings(Request $request)
+    {
         $resviceRatings = Rating::where('service_id', '!=', null)
-        ->where('service_diagnosis_by', null)->get();
+            ->where('service_diagnosis_by', null)->get();
         return view('admin.ratings.service_rating', compact('resviceRatings'));
     }
-
-
 }
