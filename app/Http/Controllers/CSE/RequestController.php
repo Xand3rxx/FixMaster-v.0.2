447de36f6052acfb3ca5eede82d40ac453d78c00
Adeolu <osunsamiadeolu@gmail.com>
2021-04-07 11:39:28 -0700

    Project updated

--- app/Http/Controllers/CSE/RequestController.php
+++ app/Http/Controllers/CSE/RequestController.php
@@ -72,19 +72,14 @@ class RequestController extends Controller
                         return $query->where('recurrence', 'yes')->whereBetween('phase', [1, 20]);
                     })->get(['id', 'uuid', 'name']),
             ]);
-            if ($service_request_progresses->sub_status_id >= 17) {
+            if ($service_request_progresses->sub_status_id >= 10) {
                 // find the Issued RFQ
                 $service_request->load(['rfqs' => function ($query) {
-                    $query->where('status','Awaiting')->where('accepted', 'no')->with('rfqBatches','rfqSupplier', 'rfqSupplier.supplier')->first();
+                    $query->where('status', 'Awaiting')->where('accepted', 'No')->with('rfqBatches', 'rfqSupplier', 'rfqSupplier.supplier')->first();
                 }]);
-                    // dd($service_request->load(['rfqs' => function ($query) {
-                    //     $query->where('status','Awaiting')->where('accepted', 'no')->with('rfqBatches','rfqSupplier', 'rfqSupplier.supplier')->first();
-                    // }]), $service_request['rfqs'][0]['rfqSupplier']);
+                // dd($service_request['rfqs']);
             }
-            // dd($variables);
         }
-
-
         return view('cse.requests.show', $variables);
     }
 
