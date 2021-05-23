3f1947520897a497a9373a696876f490407d6d5a
Adeolu <osunsamiadeolu@gmail.com>
2021-05-23 10:37:57 -0700

    Email Verification
    Handle email verification and resend

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Traits\EmailVerification;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+
 // use Illuminate\Foundation\Auth\VerifiesEmails;
 
 class VerificationController extends Controller
--- routes/frontend.php
+++ routes/frontend.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\Frontend\ApplicantsForm\CSEFormController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ServiceRequest\ClientDecisionController;
@@ -29,18 +30,23 @@ Auth::routes([
     'verify'   => false,  // for email verification
 ]);
 
+Route::get('email/verify', [VerificationController::class, 'show'])->middleware('auth')->name('verification.notice');
+Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->middleware(['auth', 'signed'])->name('verification.verify');
+Route::post('/email/verification-notification',  [VerificationController::class, 'resend'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
-Route::get('/email/verify/{id}/{hash}', [\App\Traits\EmailVerification::class, 'verify'])->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::get('email/verify', function () {
-    return view('auth.verify', ['email' => request()->user()->email]);
-})->middleware('auth')->name('verification.notice');
-
-Route::post('/email/verification-notification', function (\Illuminate\Http\Request $request) {
-    // Call Notification EmailVerification trait
-    $request->user()->sendEmailVerificationNotification();
-    return back()->with('status', 'Verification Email Sent');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+// Route::post('/email/verification-notification', function (\Illuminate\Http\Request $request) {
+//     // Call Notification EmailVerification trait
+//     $request->user()->sendEmailVerificationNotification();
+//     return back()->with('status', 'Verification Email Sent');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');send'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+// Route::post('/email/verification-notification', function (\Illuminate\Http\Request $request) {
+//     // Call Notification EmailVerification trait
+//     $request->user()->sendEmailVerificationNotification();
+//     return back()->with('status', 'Verification Email Sent');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+// Route::get('email/verify', function () {
+//     return view('auth.verify',);
+// })->middleware('auth')->name('verification.notice');
 
 
 Route::view('/', 'frontend.index')->name('frontend.index');
