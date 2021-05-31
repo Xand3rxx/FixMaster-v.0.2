b075dbf5182d845958fb3012f8519fc548626c0d
Adeolu <osunsamiadeolu@gmail.com>
2021-05-31 02:19:38 -0700

    Project Progress

--- resources/views/cse/requests/includes/project-progresses.blade.php
+++ resources/views/cse/requests/includes/project-progresses.blade.php
@@ -3,18 +3,12 @@
     <p class="mg-b-0">Specify the current progress of the job.</p>
     <div class="form-row mt-4">
         <div class="form-group col-md-12">
-            <select required class="form-control custom-select @error('sub_status_uuid') is-invalid @enderror"
-                name="sub_status_uuid">
+            <select required class="form-control custom-select" name="project_progress">
                 <option selected disabled value="0">Select...</option>
                 @foreach ($ongoingSubStatuses as $status)
                     <option value="{{ $status->uuid }}">{{ $status->name }}</option>
                 @endforeach
             </select>
-            @error('sub_status_uuid')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
         </div>
     </div>
 </section>
